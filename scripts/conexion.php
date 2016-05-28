83577f8fea506e1d10007711bba71762aa33c8f8
= <gonen.rt@gmail.com>
2016-05-28 12:17:59 -0400

    Conexion a bd local y datos bd

--- scripts/conexion.php
+++ scripts/conexion.php
@@ -1,7 +1,22 @@
  <?php
 
-//CONEXION BD SERVIDOR
+//CONEXION BD LOCAL
 
+$servername = "localhost";
+$dbname = "sdproductos";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Conexion exitosa";
+}catch(PDOException $e){
+    echo "Conexion fallida: " . $e->getMessage();
+}
+
+//CONEXION BD SERVIDOR
+/*
 $dsn = "bonito";
 $servername = "10.3.132.38";
 $dbname = 'bonito';
@@ -11,7 +26,9 @@ $password = "123";
 $cid=odbc_connect($dsn, $username, $password);
 
 if (!$cid){
-	exit("<strong>Ya ocurrido un error tratando de conectarse con el origen de datos.</strong>");
+	exit("<strong>Ha ocurrido un error tratando de conectarse con el origen de datos.</strong>");
+}else{
+  echo("<strong>Conectado con el origen de datos.</strong>");
 }
-
+*/
 ?>
--- scripts/insertarproductos.php
+++ scripts/insertarproductos.php
@@ -5,35 +5,60 @@ require('conexion.php');
 $categoria = $_GET['cat'];
 //echo "<h3>La categoria es: ".$categoria."<h3>";
 
+function cargarCategoria($categoria,$conn){
+
+  $stmt = $conn->prepare("SELECT * FROM productos WHERE categoria=:cat");
+  $stmt->bindParam(':cat', $categoria);
+  $stmt->execute();
+
+  while($row = $stmt->fetch()){
+      cargarProductos($row['id'],$row['nombre'],$row['precio'],$row['descripcion'],$row['imagen']);
+  }
+}
+
+
+/*
 function cargarCategoria($categoria,$cid){
 
-  $sql="Select * from Productos where nom_categoria =".$categoria;
+  if($categoria == 'computacion'){
+    $codigo = 110;
+  }
+
+  if($categoria == 'electronica'){
+    $codigo = 120;
+  }
+
+  if($categoria == 'telefonia'){
+    $codigo = 130;
+  }
+
+  $sql="Select * from Productos where seccion =".$codigo;
 
   $result=odbc_exec($cid,$sql)or die(exit("Error en odbc_exec"));
   //print odbc_result_all($result,"border=1");
 
   while ($datos=odbc_fetch_row($result)){
 
-    echo $datos['categoria']."<br>";
+    echo $datos['producto']."<br>";
   }
 
-}
+}*/
 
 function cargarProductos($idProducto,$nombre,$precio,$descripcion,$imagen){
 
   echo '
       <div class="item  col-xs-4 col-lg-4" id="'.$idProducto.'">
           <div class="thumbnail">
-              <img class="group list-group-image" src="'.$imagen.'"/>
+              <img class="group list-group-image img-responsive" id="vista-previa" src="'.$imagen.'"/>
               <div class="caption">
-                  <h4 class="group inner list-group-item-heading">Product title</h4>
+                  <h4 class="group inner list-group-item-heading">'.$nombre.'</h4>
                   <p class="group inner list-group-item-text">'.$descripcion.'</p>
                   <div class="row">
                       <div class="col-xs-12 col-md-6">
-                          <p class="lead">'.$precio.'</p>
+                          <p class="lead">$ '.$precio.'</p>
                       </div>
                       <div class="col-xs-12 col-md-6">
-                          <a class="btn btn-success" href="http://www.jquery2dotnet.com">Add to cart</a>
+                          <a class="btn btn-success" href="#">Comprar</a>
                       </div>
                   </div>
               </div>
@@ -42,6 +67,6 @@ function cargarProductos($idProducto,$nombre,$precio,$descripcion,$imagen){
   ';
 }
 
-cargarCategoria($categoria,$cid);
-
+//cargarCategoria($categoria,$cid); //SERVIDOR
+cargarCategoria($categoria,$conn); //LOCAL
 ?>
