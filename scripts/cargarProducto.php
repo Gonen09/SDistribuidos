fdefdc3e4e1d96ecb98dd395b8b03386e68c9405
= <gonen.rt@gmail.com>
2016-06-01 11:13:43 -0400

    Terminada busqueda por nombre, en modo local

--- scripts/cargarProducto.php
+++ scripts/cargarProducto.php
@@ -3,14 +3,24 @@
   require('vistaProducto.php');
 
   $producto = $_GET['pro'];
-  echo "<h3>El producto es: ".$producto."<h3>";
+  //echo "<h3>El producto es: ".$producto."<h3>";
 
   //LOCAL
   function cargarBusqueda($producto,$conn){
-    //$stmt = $conn->prepare("SELECT * FROM productos WHERE CONTAINS (nombre,':nombre')");
-    $stmt = $conn->prepare("SELECT * FROM productos WHERE nombre LIKE ':%nombre%'");
-    $stmt->bindParam(':nombre', $producto);
+
+    $producto = trim($producto);
+    $query = "SELECT * FROM productos WHERE nombre LIKE '%".$producto."%'";
+    $stmt = $conn->prepare($query);
     $stmt->execute();
+    
+    //bindParam
+    /*
+    $term = "'%".$producto."%'";
+    echo $term;
+    $stmt = $conn->prepare("SELECT * FROM productos WHERE nombre LIKE :nombre");
+    $stmt->bindParam(':nombre', $term);
+    $stmt->execute();
+    */
 
     while($row = $stmt->fetch()){
         cargarProductos($row['id'],$row['nombre'],$row['precio'],$row['descripcion'],$row['imagen']);
